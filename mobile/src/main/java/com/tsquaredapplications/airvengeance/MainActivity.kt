82c8643f865cee3242a93121e512a4a39e070c01
the-raccoondog <dylan1406lewis@gmail.com>
2018-09-12 12:49:35 -0400

    Added some basic init of UI

--- mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
+++ mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
@@ -2,11 +2,15 @@ package com.tsquaredapplications.airvengeance
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        temperature_tv.text = getString(R.string.temperature_reading, "24")
+        humidity_tv.text = getString(R.string.humidity_reading, "56")
     }
 }
