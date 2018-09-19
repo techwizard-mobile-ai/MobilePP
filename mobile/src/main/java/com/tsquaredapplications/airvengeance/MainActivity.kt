4da595837d4cf01bdffbe19723b5f8c9039e46aa
the-raccoondog <dylan1406lewis@gmail.com>
2018-09-19 09:48:14 -0400

    Removed preferences starting to go back and branch for class requirements.

--- mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
+++ mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tsquaredapplications.airvengeance
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,21 @@ class MainActivity : AppCompatActivity() {
 
         temperature_tv.text = getString(R.string.temperature_reading, "24")
         humidity_tv.text = getString(R.string.humidity_reading, "56")
+        pressure_tv.text = getString(R.string.pressure_reading, "20")
+        pm2_5_tv.text = getString(R.string.pm25_reading, "20")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_settings -> {
+
+            }
+        }
+        return true
     }
 }
