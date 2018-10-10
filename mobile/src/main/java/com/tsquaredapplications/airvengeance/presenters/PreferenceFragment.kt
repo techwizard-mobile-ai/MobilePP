8003a078d66ce6c9c66b56f76507a452d180fdf1
the-raccoondog <dylan1406lewis@gmail.com>
2018-10-10 11:04:22 -0400

    Added preference option for Zip Code entry for Weather API.

--- mobile/src/main/java/com/tsquaredapplications/airvengeance/presenters/PreferenceFragment.kt
+++ mobile/src/main/java/com/tsquaredapplications/airvengeance/presenters/PreferenceFragment.kt
@@ -3,10 +3,12 @@ package com.tsquaredapplications.airvengeance.presenters
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceManager
 import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat
 import com.tsquaredapplications.airvengeance.R
 import com.tsquaredapplications.airvengeance.R.string.minimum_temp
+import com.tsquaredapplications.airvengeance.R.string.preferences
 
 
 class PreferenceFragment : PreferenceFragmentCompat() {
@@ -20,9 +22,10 @@ class PreferenceFragment : PreferenceFragmentCompat() {
         val maxHumid = findPreference(getString(R.string.maximum_humid))
         val minPress = findPreference(getString(R.string.minimum_press))
         val maxPress = findPreference(getString(R.string.maximum_press))
+        val zipCode = findPreference(getString(R.string.zip_code))
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        //minTemp.setOnPreferenceChangeListener(new OnPreferenceChangeListener())
+
 
         val mT = prefs.getString(getString(R.string.minimum_temp),"10")
         minTemp.setTitle("Minimum Temperature : $mT" )
@@ -43,6 +46,8 @@ class PreferenceFragment : PreferenceFragmentCompat() {
         val MH = prefs.getString(getString(R.string.maximum_humid),"40")
         maxHumid.setTitle("Maximum Humiity : $MH" )
 
+        val zip = prefs.getString(getString(R.string.zip_code),"14127")
+        zipCode.setTitle("Zip Code : $zip")
 
     }
 
