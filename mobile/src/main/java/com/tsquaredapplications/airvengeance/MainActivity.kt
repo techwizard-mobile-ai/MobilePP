026d37559b9507027c5c1af03beea305296f60ca
the-raccoondog <dylan1406lewis@gmail.com>
2018-10-22 16:59:30 -0400

    Merged History graph branch with dev_sprint_2

--- mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
+++ mobile/src/main/java/com/tsquaredapplications/airvengeance/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = findNavController(R.id.nav_host)
-
         setupNavigation()
     }
 
